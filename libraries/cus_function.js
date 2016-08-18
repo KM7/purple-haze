1c5efeb0c5e284b684fcaf71b3ce16441176fedc
Kenny Ma <kenny.uk@me.com>
2016-08-18 11:34:59 -0700

    Updated javascript and function

--- libraries/cus_function.js
+++ libraries/cus_function.js
@@ -1,8 +1,6 @@
-function get_update_JSON(){
 var return_val;
+function get_update_JSON(){
 $.ajax({url: "https://api.mlab.com/api/1/databases/p-fire/collections/purplegarage?apiKey=ge3hUcuINpHsOkUhoayG7tL2vTpkrETR", success: function(result){
         return_val=result;
     }});
-
-return return_val;
 }
